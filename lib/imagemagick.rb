1990689697202702a3b650b2bdb590b6b3e3877f
pjkelly <pj@crushlovely.com>
2010-09-01 10:48:42 -0700

    Syntax cleanup.

--- lib/imagemagick.rb
+++ lib/imagemagick.rb
@@ -1,21 +1,12 @@
 module Imagemagick
 
-  # Define options for this plugin via the <tt>configure</tt> method
-  # in your application manifest:
+  # Just call the recipe:
   #
-  #   configure(:imagemagick => {:foo => true})
-  #
-  # Then include the plugin and call the recipe(s) you need:
-  #
-  #  plugin :imagemagick
   #  recipe :imagemagick
   def imagemagick(options = {})
-    %w(
-      imagemagick
-      libmagick9-dev
-    ).each do |p|
+    %w(imagemagick libmagick9-dev).each do |p|
       package p, :ensure => :installed, :before => exec('rails_gems')
     end
   end
-  
+
 end
